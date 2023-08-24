9c9b551d1a35ecc870cb7d038b50796cb6a84db8
Yağmur K <yagmurrttk@gmail.com>
2023-08-24 15:10:50 +0300

    .

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adını boş geçemezsiniz");
+            RuleFor(x => x.WriterSurName).NotEmpty().WithMessage("Yazar soyadını boş geçemezsiniz");
+            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkında kısmını boş geçemezsiniz");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen en az 2 karakter giriniz.");
+            RuleFor(x => x.WriterSurName).MaximumLength(50).WithMessage("Lütfen 50 karakterden fazla değer girişi yapmayınız.");
+            RuleFor(x => x.WriterAbout).Must(a=>a.ToLower().Contains("a")).WithMessage("Lütfen en az 1 tane a harfi içeren değer girişi yapmayınız.");
+        }
+    }
+}
