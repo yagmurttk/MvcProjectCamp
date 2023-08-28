c24642704ec917f3029e78cccadc6979da706560
Yağmur K <yagmurrkeserr1@gmail.com>
2023-08-28 17:01:04 +0300

    55. Ders Sonu♥ Heading  layouts

--- MvcProjeKampi/Controllers/HeadingController.cs
+++ MvcProjeKampi/Controllers/HeadingController.cs
@@ -47,5 +47,40 @@ namespace MvcProjeKampi.Controllers
             hm.HeadingAdd(p);
             return RedirectToAction("Index");
         }
+        [HttpGet]
+        public ActionResult EditHeading(int id)
+        {
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }).ToList();
+            ViewBag.vlc = valuecategory;
+            var HeadingValue = hm.GetByID(id);
+            return View(HeadingValue);
+        }
+        [HttpPost]
+        public ActionResult EditHeading(Heading p)
+        {
+            hm.HeadingUpdate(p);
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteHeading(int id)
+        {
+            var HeadingValue = hm.GetByID(id);
+            if (HeadingValue.HeadingStatus == true)
+            {
+                HeadingValue.HeadingStatus = false;
+                hm.HeadingDelete(HeadingValue);
+            }
+            else
+            {
+                HeadingValue.HeadingStatus = true;
+                hm.HeadingDelete(HeadingValue);
+            }
+            return RedirectToAction("Index");
+        }
+
     }
 }
