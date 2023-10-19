53c7b3a88994183e707343641243b54e8a6d1d3b
Yağmur K <yagmurrttk@gmail.com>
2023-10-19 13:45:27 +0300

    77.Ders Sonu ~ Writer Login işlemleri

--- MvcProjeKampi/Controllers/LoginController.cs
+++ MvcProjeKampi/Controllers/LoginController.cs
@@ -32,7 +32,27 @@ namespace MvcProjeKampi.Controllers
             {
                 return RedirectToAction("Index");
             }
+        }
+        [HttpGet]
+        public ActionResult WriterLogin()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult WriterLogin(Writer p)
+        {
+            Context c = new Context();
+            var writeruserinfo = c.Writers.FirstOrDefault(x => x.WriterMail == p.WriterMail && x.WriterPassword == p.WriterPassword);
+            if (writeruserinfo != null)
+            {
+                FormsAuthentication.SetAuthCookie(writeruserinfo.WriterMail, false);
+                Session["WriterMail"] = writeruserinfo.WriterMail;
+                return RedirectToAction("MyContent", "WriterPanelContent");
+            }
+            else
+            {
+                return RedirectToAction("WriterLogin");
+            }
         }
     }
 }
